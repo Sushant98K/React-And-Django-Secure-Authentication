e898a60e188b6f78656167e7afd93e70a674526d
Sushant Kate <47480733+Sushant98K@users.noreply.github.com>
2025-03-08 01:07:30 +0530

    Logout api Done

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import get_notes, CustomTokenObtainPairView, CustomRefreshTokenView
+from .views import get_notes, CustomTokenObtainPairView, CustomRefreshTokenView, logout
 
 urlpatterns = [
     path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),   #access token
     path('token/refresh/', CustomRefreshTokenView.as_view(), name='token_refresh'),  #refresh token
-    path('notes/', get_notes)
+    path('notes/', get_notes),
+    path('logout/', logout)
 ]
